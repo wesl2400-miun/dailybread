aff5a220965e1f6b7f7fa4cd878f2c602ced4193
= <=>
2026-03-12 12:06:42 +0100

    Just a test

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from "vite";
 import sitemap from "vite-sitemap";
 
 export default defineConfig({
-  base: 'dailybread',
+  base: '/dailybread',
   plugins: [
     sitemap({
       base: '/',
+      urls: [
+        { loc: '/', priority: 1.0, changefreq: 'yearly' }
+      ],
       filename: 'sitemap.xml'
     })
   ]
